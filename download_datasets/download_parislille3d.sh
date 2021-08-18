759c8c64b1e6e633b3b27d0421b0968d64ce4909
porteratzo <omar@aifi.com>
2021-08-17 20:31:31 -0500

    f

--- download_datasets/download_parislille3d.sh
+++ download_datasets/download_parislille3d.sh
@@ -3,13 +3,14 @@ BASE_DIR=datasets/Paris_Lille3D
 
 mkdir -p $BASE_DIR
 
-export url_train="https://cloud.mines-paristech.fr/index.php/s/JhIxgyt0ALgRZ1O/download?path=%2F&files=training_10_classes"
-export url_test="https://cloud.mines-paristech.fr/index.php/s/JhIxgyt0ALgRZ1O/download?path=%2F&files=test_10_classes"
+export url_train="https://cloud.mines-paristech.fr/index.php/s/JhIxgyt0ALgRZ1O/download?path=%2Ftraining_50_classes"
+#export url_test="https://cloud.mines-paristech.fr/index.php/s/JhIxgyt0ALgRZ1O/download?path=%2Ftraining_50_classes"
+
 
 wget -c -N -O $BASE_DIR'/training_10_classes.zip'  $url_train
-wget -c -N -O $BASE_DIR'/test_10_classes.zip' $url_test
+#wget -c -N -O $BASE_DIR'/test_10_classes.zip' $url_test
 
 cd $BASE_DIR
 
-unzip test_10_classes.zip
-unzip training_10_classes.zip
+#unzip test_10_classes.zip
+unzip training_50_classes.zip
