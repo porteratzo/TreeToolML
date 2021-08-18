1f2d34c58310b01203218229aedb6d20beba0992
porteratzo <omar@aifi.com>
2021-08-17 19:36:51 -0500

    a

--- download_datasets/download_all.sh
+++ download_datasets/download_all.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-cd download_datasets
-bash download_lucid.sh
-bash download_opentree.sh
-bash download_parislille3d.sh
-bash download_toronto3d.sh
+bash download_datasets/download_lucid.sh
+bash download_datasets/download_opentree.sh
+bash download_datasets/download_parislille3d.sh
+bash download_datasets/download_toronto3d.sh
