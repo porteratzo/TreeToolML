150622ebef5451467852be2a77d61c40bb982746
porteratzo <omar@aifi.com>
2021-08-23 01:05:34 -0500

    g

--- IndividualTreeExtraction/backbone_network/Loss_torch.py
+++ IndividualTreeExtraction/backbone_network/Loss_torch.py
@@ -14,7 +14,7 @@ def slack_based_direction_loss(pre_direction, gt_direction, sigma=0.955):
     Error Slack-based Direction Loss
     '''
     pre_direction = pre_direction.permute(0,2,1)
-    gt_direction = f.normalize(gt_direction.float(), dim=2, eps=1e-20)
+    gt_direction = f.normalize(gt_direction, dim=2, eps=1e-20)
     pre_direction = f.normalize(pre_direction, dim=2, eps=1e-20)
 
     loss = sigma-torch.sum(torch.multiply(pre_direction, gt_direction), dim=2)
@@ -30,7 +30,7 @@ def direction_loss(pre_direction, gt_direction):
     Plain Direction Loss
     '''
     pre_direction = pre_direction.permute(0,2,1)
-    gt_direction = f.normalize(gt_direction.float(), dim=2, eps=1e-20)
+    gt_direction = f.normalize(gt_direction, dim=2, eps=1e-20)
     pre_direction = f.normalize(pre_direction, dim=2, eps=1e-20)
     loss = -torch.mean(torch.sum(torch.multiply(pre_direction, gt_direction), dim=2))
     return loss
