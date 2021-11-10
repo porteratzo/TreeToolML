54371b79e6ff8e7c959ac83c12b362c7282b772c
porteratzo <omar@aifi.com>
2021-11-10 06:29:12 -0600

    f

--- scripts/datapreparation/download_datasets/download_all.sh
+++ scripts/datapreparation/download_datasets/download_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-bash download_datasets/download_lucid.sh
-bash download_datasets/download_opentree.sh
-bash download_datasets/download_parislille3d.sh
-bash download_datasets/download_toronto3d.sh
+bash scripts/datapreparation/download_datasets/download_lucid.sh
+bash scripts/datapreparation/download_datasets/download_opentree.sh
+bash scripts/datapreparation/download_datasets/download_parislille3d.sh
+bash scripts/datapreparation/download_datasets/download_toronto3d.sh
